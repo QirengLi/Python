d1d9af242cc40609f3669d569795b5e67c78ebc5
Calvin Li <calvinli12345@gmail.com>
2022-09-16 00:06:43 -0500

    added comments

--- Leetcode/217 Contains Duplicate.py
+++ Leetcode/217 Contains Duplicate.py
@@ -1,9 +1,17 @@
 def containsDuplicate(nums):
 
-    #
+    # Space Complexity: O(n)
+    # Time Complexity: O(n)
+
+    # hashset or dictionary for optimal lookup
     hashset = set()
+
+    # First found duplicate will end function and return true since it satisfies the requirements
+    # and won't need to continue the for loop
     for i in nums:
         if i in hashset:
             return True
         hashset.add(i)
+
+    # Will return false once for loop goes through every item therefore not containing any duplicates
     return False
