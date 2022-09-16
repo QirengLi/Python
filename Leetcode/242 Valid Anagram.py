97dde674cf802baedbffa835ee7850bb5e808992
Calvin Li <calvinli12345@gmail.com>
2022-09-16 01:06:16 -0500

    added a few comments to 242

--- Leetcode/242 Valid Anagram.py
+++ Leetcode/242 Valid Anagram.py
@@ -29,24 +29,25 @@ def isAnagram(s, t):
 # Below function doesn't remove anything from the dictionary but instead, subtracts the count
 # of the chars and then checks all the values to see if it equals 0
 
+# both solutions are space and time complexity of O(n)
 
-# def isAnagram(s, t):
-#     str_dict = {}
-#
-#     for i in s:
-#         if i in str_dict:
-#             str_dict[i] += 1
-#         else:
-#             str_dict[i] = 1
-#
-#     for j in t:
-#         if j in str_dict:
-#             str_dict[j] -= 1
-#         else:
-#             return False
-#
-#     for vals in str_dict.values():
-#         if vals != 0:
-#             return False
-#
-#     return True
+def isAnagram(s, t):
+    str_dict = {}
+
+    for i in s:
+        if i in str_dict:
+            str_dict[i] += 1
+        else:
+            str_dict[i] = 1
+
+    for j in t:
+        if j in str_dict:
+            str_dict[j] -= 1
+        else:
+            return False
+
+    for vals in str_dict.values():
+        if vals != 0:
+            return False
+
+    return True
