08a3e40bcb88c89cb35191fbea42f4cdd8e1c237
Calvin Li <calvinli12345@gmail.com>
2022-09-16 00:03:46 -0500

    added solution

--- scratch.py
+++ scratch.py
@@ -1,2 +1,14 @@
-def printNew():
-    print("hello")
+
+
+def maximum_pairwise_product(numbers):
+    dict = {"first_max": 0, "second_max": 0}
+
+    for i in range(len(numbers)):
+        if numbers[i] > dict["first_max"]:
+            dict["first_max"] = numbers[i]
+        elif numbers[i] > dict["second_max"]:
+            dict["second_max"] = numbers[i]
+    print(dict.values())
+
+
+maximum_pairwise_product([7, 5, 14, 2, 8, 8, 5214213412, 1, 2, 3])
