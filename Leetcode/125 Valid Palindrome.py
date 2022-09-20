1b5bd32843d856c2e7b405091bb21813a513d988
Calvin Li <calvinli12345@gmail.com>
2022-09-20 00:06:31 -0500

    added optimized method 125

--- Leetcode/125 Valid Palindrome.py
+++ Leetcode/125 Valid Palindrome.py
@@ -1,4 +1,3 @@
-
 # Brute force method
 
 def isPalindrome(s):
@@ -18,3 +17,20 @@ def isPalindrome(s):
 
 # Optimized method
 
+def isPalindrome(s):
+
+    l = 0
+    r = len(s) - 1
+
+    while l < r:
+        if not s[l].isalnum():
+            l += 1
+        elif not s[r].isalnum():
+            r -= 1
+        else:
+            if s[l].lower() != s[r].lower():
+                return False
+            else:
+                l += 1
+                r -= 1
+    return True
