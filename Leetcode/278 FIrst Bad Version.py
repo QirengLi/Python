e537c989a048299fb08769593f9d57e17f632b49
Calvin Li <calvinli12345@gmail.com>
2022-09-23 01:48:13 -0500

    few edits to 278

--- Leetcode/278 FIrst Bad Version.py
+++ Leetcode/278 FIrst Bad Version.py
@@ -1,9 +1,9 @@
 def firstBadVersion(n):
-    low = 0
-    high = n - 1
+    low = 1 # use 1 here because it's based on "versions" and realistically wouldn't have a version 0
+    high = n
 
     while low < high:
-        mid = low + (high - low) // 2
+        mid = low + (high - low) // 2 # adding low after calculating "true mid" (( high-low) // 2) helps avoid overflow
 
         if isBadVersion(mid): #Assuming IsBadVersion(version) checks and returns bool value if version is bad
             high = mid
