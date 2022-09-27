17697f18f278a3a2092a7897bcc713672dc60c7d
Calvin Li <calvinli12345@gmail.com>
2022-09-26 20:25:58 -0500

    added solution for 121

---
+++ Leetcode/121 Best Time to Buy and Sell Stock.py
@@ -0,0 +1,15 @@
+def maxProfit(prices):
+    left = 0
+    right = 1
+    max_profit = 0
+
+    while right < len(prices):
+        current_profit = prices[right] - prices[left]
+
+        if prices[left] < prices[right]:
+            max_profit = max(max_profit, current_profit)
+        else:
+            left = right
+        right += 1
+
+    return max_profit
