53da7f676d9b0801405ccc5f45a5da0c0b8db26f
Calvin Li <calvinli12345@gmail.com>
2022-09-29 02:54:44 -0500

    added solutions for 104 and 111

--- Leetcode/DS Practice/Binary Tree.py
+++ Leetcode/DS Practice/Binary Tree.py
@@ -3,3 +3,27 @@ class Node:
         self.val = val
         self.left = None
         self.right = None
+
+
+# left branch -> current node -> right branch
+def in_order_traversal(root: Node):
+    if root is not None:
+        in_order_traversal(root.left)
+        print(root.val)
+        in_order_traversal(root.right)
+
+
+# current node -> left branch -> right branch
+def pre_order_traversal(root: Node):
+    if root is not None:
+        print(root.val)
+        pre_order_traversal(root.left)
+        pre_order_traversal(root.right)
+
+
+# left branch -> right branch -> current node
+def post_order_traversal(root: Node):
+    if root is not None:
+        post_order_traversal(root.left)
+        post_order_traversal(root.right)
+        print(root.val)
