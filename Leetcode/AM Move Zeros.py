fcd1075de1a5fc02eda51f6fb5b3e372ca4f27d5
Calvin Li <calvinli12345@gmail.com>
2022-09-30 01:23:03 -0500

    added AlgoMonster problem solution

---
+++ Leetcode/AM Move Zeros.py
@@ -0,0 +1,14 @@
+# Given an array of integers, move all the 0s to the back of the array
+# while maintaining the relative order of the non-zero elements.
+# Do this in-place using constant auxiliary space.
+
+# Input: [1, 0, 2, 0, 0, 7]
+# Output: [1, 2, 7, 0, 0, 0]
+
+def move_zeros(nums):
+    slow = 0
+    for fast in range(len(nums)):
+        if nums[fast] != 0:
+            nums[slow], nums[fast] = nums[fast], nums[slow]
+            slow += 1
+
