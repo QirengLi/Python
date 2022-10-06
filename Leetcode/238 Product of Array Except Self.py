cbb73cc72bc0747995cbb2dc667bdc5a527fe4ac
Calvin Li <calvinli12345@gmail.com>
2022-10-05 23:45:35 -0500

    added part two of 238

--- Leetcode/238 Product of Array Except Self.py
+++ Leetcode/238 Product of Array Except Self.py
@@ -1,4 +1,6 @@
 def productExceptSelf(nums):
+
+# Left array to contain
     left_nums, right_nums, res = [1] * len(nums), [1] * len(nums), [1] * len(nums)
 
     for i in range(1, len(nums)):
@@ -10,4 +12,23 @@ def productExceptSelf(nums):
     for i in range(len(nums)):
         res[i] = left_nums[i] * right_nums[i]
 
+    return res
+# Time Complexity O(n)
+# Space Complexity O(n)
+
+# More optimized version that uses a better space complexity
+# Time Complexity O(n)
+# Space Complexity O(1)
+
+def productExceptSelfTwo(nums):
+    res = [1] * (len(nums))
+
+    left = 1
+    for i in range(len(nums)):
+        res[i] = left
+        left *= nums[i]
+    right = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= right
+        right *= nums[i]
     return res
